0f07553d231593d9f076a73df14e8d6a602076ee
Stepan <stepanmaslov@gmail.com>
2023-06-09 15:35:21 +0300

    no work, finish interface

---
+++ Sem3_4/StudentApp/src/StudentDomen/PersonComparator.java
@@ -0,0 +1,11 @@
+package StudentDomen;
+
+import java.util.Comparator;
+
+public class PersonComparator<T extends Person> implements Comparator<T> {
+
+    @Override
+    public int compare(T o1, T o2) {
+            return o1.getFirstName().compareTo(o2.getFirstName());
+    }    
+}
---
+++ Sem3_4/StudentApp/src/StudentDomen/StudentGroup.java
@@ -0,0 +1,44 @@
+package StudentDomen;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class StudentGroup implements Iterable<Student> {
+    List<Student> group;
+    private int idGroup;
+
+    public StudentGroup(List<Student> group, int idGroup) {
+        this.group = group;
+        this.idGroup = idGroup;
+    }
+
+    public List<Student> getGroup() {
+        return group;
+    }
+
+    public void setGroup(List<Student> group) {
+        this.group = group;
+    }
+
+    public int getIdGroup() {
+        return idGroup;
+    }
+
+    public void setIdGroup(int idGroup) {
+        this.idGroup = idGroup;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentGroup{" +
+                "group=" + group +
+                ", idGroup=" + idGroup +
+                '}';
+    }
+
+    @Override
+    public Iterator<Student> iterator() {
+        return new SGIterator(group);
+    }
+
+}
